8a7a95e102fc7a537010a371c0a2ace0181b0d2c
= <mpenaloza@uc.cl>
2023-09-20 00:13:38 -0300

    feat: Update README.md

--- search_problems/core/foundations/problems/sokoban/sokoban_problems.py
+++ search_problems/core/foundations/problems/sokoban/sokoban_problems.py
@@ -1,21 +1,5 @@
 import copy
 
-def generate_possible_goals(goal):
-  goals = []
-  row_index = 0
-  for row in goal:
-    column_index = 0
-    for column in row:
-      if column == " ":
-        new_goal = copy.deepcopy(goal)
-        new_goal[row_index][column_index] = "A"
-        goals.append(new_goal)
-      column_index += 1
-    row_index += 1
-  return goals
-
-
-
 sokoban_problems = {
   "easy_1": {
     "init_agent_coords": [0, 3],
--- search_problems/sokoban.py
+++ search_problems/sokoban.py
@@ -1,5 +1,4 @@
 from solver import create_solver
   
-#sokoban_solver = create_solver("sokoban", "easy_1", "bfs", None)
 sokoban_solver = create_solver("sokoban", "game", "best_first", "boxes_out_of_goal")
 sokoban_solver = create_solver("sokoban", "game", "best_first", "manhattan_distance")
